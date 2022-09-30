9db33d90e9369dbbcc44db62cc8afae5047f2389
popudev <phuthinh53.it@gmail.com>
2022-09-30 11:02:10 +0700

    login social medio logic new p3

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -7,6 +7,7 @@ const httpRequest = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'content-type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
   },
   paramsSerializer: (params) => queryString.stringify(params),
 });
